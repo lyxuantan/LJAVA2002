2d1290a0ef5d9f59b5a80190a403ed39dfbaa78a
lyxuantan <lyxuantan1999@gmail.com>
2020-07-03 19:59:14 +0700

    first commit

---
+++ BaiTapBuoi2/src/com/tan/baitapbuoi2/BaiTapBuoi2.java
@@ -0,0 +1,23 @@
+package com.tan.baitapbuoi2;
+
+public class BaiTapBuoi2 {
+
+	public static void main(String[] args) {
+		System.out.println("Tổng các số nhỏ hơn 100"); 
+		int sum=0;
+		for(int i=0;i<100;i++)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+		System.out.println("Giai thừa của 10");
+		int giaithua=1;
+		for(int i=1;i<=10;i++)
+		{
+			giaithua*=i;
+		}
+		System.out.println(giaithua);
+
+	}
+
+}
